b84970a5562978e7fd0967b2bf30433cc3fa48fd
Kaden Thomas <thomas.kaden4@outlook.com>
2018-10-30 00:15:13 -0700

    Fix xorful

--- cryptography/xorful/app.py
+++ cryptography/xorful/app.py
@@ -11,7 +11,7 @@ key = os.environ["KEY"]
 def root():
     if request.method == "POST":
         plaintext = request.form["plaintext"]
-        encrypted = base64.b64encode(encrypt(plaintext, key))
+        encrypted = encrypt(plaintext, key)
         return f"""
             <html>
             <head>
